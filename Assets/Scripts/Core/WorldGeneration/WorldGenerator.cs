79d3216f2306ed64b1be610c0221624da29f90ce
Daniel <golo7ov.danil@gmail.com>
2022-10-21 19:50:04 +0200

    Added spawn probability to World Generation process

--- Assets/Scripts/Core/WorldGeneration/WorldGenerator.cs
+++ Assets/Scripts/Core/WorldGeneration/WorldGenerator.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using Core.Miscellaneous;
 using PlayerInteractable;
@@ -7,41 +6,58 @@ using Random = UnityEngine.Random;
 
 namespace Core.WorldGeneration
 {
+
+    [System.Serializable]
+    public class PlanetTypes
+    {
+        public string Name; // debug - remove before build
+        
+        public Planet planetPrefab;
+        
+        [Range(0f, 100f)] public float spawnProbability = 100f;
+    }
+    
     public class WorldGenerator : MonoBehaviour
     {
-        [SerializeField] private Planet[] planetsType;
-            
-        [SerializeField]
-        [Range(0.7f, 3f)]
-        private float spawnDensity = 1f;
-            
-        private List<Planet> _instantiatedPlanets;
+        [SerializeField] private PlanetTypes[] planetsType;
 
-        private readonly int[] _spawnWeight = Enumerable.Range(1, 100).ToArray();
-        private readonly int PLANET_SPAWN_ITERATIONS = 2000;
+        [SerializeField] [Range(0.7f, 3f)] private float spawnDensity = 1f;
+        
+        private const int PLANET_SPAWN_ITERATIONS = 500;
         
         private void Start()
         {
-            _instantiatedPlanets = new List<Planet>();
             GeneratePlanetsInRandomPoints();
         }
-        
-        
+
         private void GeneratePlanetsInRandomPoints()
         {
+        
             for (int i = 0; i < PLANET_SPAWN_ITERATIONS; i++)
             {
-                int randomPlanet = Random.Range(0, planetsType.Length);
-
-                Vector3 randomPlanetPointOnMap = GenerateRandomPoint();
                 
+                Vector3 randomPlanetPointOnMap = GenerateRandomPoint();
+        
                 if (Physics.CheckSphere(randomPlanetPointOnMap, Planet.COLLIDER_RADIUS / spawnDensity)) continue;
 
-                var planet = Instantiate(planetsType[randomPlanet], randomPlanetPointOnMap, Quaternion.identity);
-                _instantiatedPlanets.Add(planet);
+                SpawnPlanetWithProbability(randomPlanetPointOnMap);
             }
         }
 
+        private void SpawnPlanetWithProbability(Vector3 randomPointOnMap)
+        {
+            for (int i = 0; i < planetsType.Length; i++)
+            {
+                float rnd = Random.Range(0.1f, 100);
+
+                if (rnd <= planetsType[i].spawnProbability)
+                {
+                    Instantiate(planetsType[i].planetPrefab, randomPointOnMap, Quaternion.identity);
+                    Debug.Log("Name: " + planetsType[i].Name); // debug - remove before build
+                }
+            }
+        }
+        
         private Vector3 GenerateRandomPoint()
         {
             return new Vector3(
